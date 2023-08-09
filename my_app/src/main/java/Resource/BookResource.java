66141c132dc6054b067826f26bc004306fed7c29
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2023-08-09 10:29:25 +0530

    initial commit

--- my_app/src/main/java/Resource/BookResource.java
+++ my_app/src/main/java/Resource/BookResource.java
@@ -2,66 +2,21 @@ package Resource;
 
 import Entity.Book;
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import org.bson.types.ObjectId;
+import org.eclipse.microprofile.openapi.annotations.Operation;
 
-import javax.enterprise.context.ApplicationScoped;
 import java.util.List;
 
 @Path("/books")
-//@ApplicationScoped // or other appropriate scope
-
 public class BookResource {
-//    @Inject
-//    BookRepository bookRepository;
-@GET
-@Produces(MediaType.APPLICATION_JSON)
-public List<Book> getBooks() {
-    // Use Panache to retrieve and return list of books
-    return Book.listAll();
-}
-//
-//    @GET
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public List<Book> getAllBooks() {
-//        return bookRepository.listAll();
-//    }
-//
-//    @GET
-//    @Path("/{id}")
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public Book getBookById(@PathParam("id") String id) {
-//        return bookRepository.findById(new ObjectId(id));
-//    }
-//
-//    @POST
-//    @Transactional
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    public void addBook(Book book) {
-//        bookRepository.persist(book);
-//    }
-//
-//    @PUT
-//    @Path("/{id}")
-//    @Transactional
-//    @Consumes(MediaType.APPLICATION_JSON)
-//    public void updateBook(@PathParam("id") String id, Book book) {
-//        Book entity = bookRepository.findById(new ObjectId(id));
-//        if (entity != null) {
-//            entity.title = book.title;
-//            entity.author = book.author;
-//            entity.pages = book.pages;
-//            entity.genres = book.genres;
-//            entity.rating = book.rating;
-//        }
-//    }
-//
-//    @DELETE
-//    @Path("/{id}")
-//    @Transactional
-//    public void deleteBook(@PathParam("id") String id) {
-//        bookRepository.deleteById(new ObjectId(id));
-//    }
+
+    @GET
+    @Operation(summary = "Get a list of books")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Book> getBooks() {
+        return Book.listAll();
+    }
 }
