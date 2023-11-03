54173186caf4e556be9ea8ca50bd823938ffe38b
Gnanesh26 <gnaneswarrao.yeddu@imaginnovate.com>
2023-11-03 19:12:30 +0530

    initial commit

--- code/code/src/main/java/org/sample/GreetingResource.java
+++ code/code/src/main/java/org/sample/GreetingResource.java
@@ -5,12 +5,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/healthCheck")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "H! , This is Quarkus..";
     }
 }
